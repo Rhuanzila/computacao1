8959aeed8dd9c84b71c8d3cd7b88cf4beab4b402
Rhuanzila <107738165+Rhuanzila@users.noreply.github.com>
2026-04-29 11:17:52 -0300

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+//Fibonnacci
+
+int main(){
+    
+    
+    
+    setlocale(LC_ALL, "Portuguese");
+    
+    int n;
+    printf("Digite um número inteiro:");
+    scanf("%d", &n);
+    
+    int fibonacci[n-1];
+    
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    
+    for(int i = 2; i < n; i++){
+        fibonnacci[i] = fibonnacci[i-1] + fibonnacci[i-2];
+    }
+    
+    for(int j = 0; j < n; j++){
+        printf("%d ", fibonnacci[j]);
+    }
+    
+    return 0;
+}
